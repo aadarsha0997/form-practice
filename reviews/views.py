bde480ff71057e670495b5410c3803b6290a19e8
Ayush <aadarsha0997@gmail.com>
2024-09-21 21:36:18 +0545

    initilizing git

---
+++ reviews/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+# Create your views here.
+
+def review(request):
+    if request.method== 'POST':
+        entered_username=request.POST['username']
+        if entered_username=="":
+         return render(request,"reviews/review.html",{
+            "error":True
+         })
+
+        print(entered_username)
+        return HttpResponseRedirect("/thanks")
+    return render(request,"reviews/review.html")
+def thank_you (request):
+    return render(request,"reviews/thank-you.html")
