66ebd2439b8b27821a34b43748c196381f107c1a
Ayush <aadarsha0997@gmail.com>
2024-09-23 22:09:40 +0545

    Adding costunised error message but dont know why not working :)

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,4 +1,7 @@
 from django import forms
 
 class ReviewForm(forms.Form):
-    user_name=forms.CharField()
+    user_name=forms.CharField(label="Your name",max_length=20,error_messages={
+        "required":"Your must not be empty",
+        "max_length":"Please Enter Shoter Name"
+    })
--- reviews/views.py
+++ reviews/views.py
@@ -8,10 +8,13 @@ from .forms import ReviewForm
 def review(request):
     if request.method== 'POST':
         form=ReviewForm(request.POST)
+
         if form.is_valid():
             print(form.cleaned_data)
-        return HttpResponseRedirect("/thanks")
-    form=ReviewForm()
+            return HttpResponseRedirect("/thanks")
+    else:
+        form=ReviewForm()
+        
     return render(request,"reviews/review.html",{
         "form":form
     })
