042f59f2f57e40338a05b27ad5cfd008caa728c3
Ayush <aadarsha0997@gmail.com>
2024-10-11 14:35:39 +0545

    Adding another field in form and saving data in database

--- reviews/forms.py
+++ reviews/forms.py
@@ -5,3 +5,5 @@ class ReviewForm(forms.Form):
         "required":"Your must not be empty",
         "max_length":"Please Enter Shoter Name"
     })
+    review_text =forms.CharField(label="Your feedback",widget=forms.Textarea,max_length=200)
+    rating=forms.IntegerField(label="Your Rating",min_value=1,max_value=5)
