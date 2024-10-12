7b7482a96f8d6faa773b82f66a6af2d806df535f
Ayush <aadarsha0997@gmail.com>
2024-10-12 20:39:07 +0545

    Extending Form from ModelForm instead of Form

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,9 +1,16 @@
-from django import forms
+from django import forms 
+from .models import Review
 
-class ReviewForm(forms.Form):
-    user_name=forms.CharField(label="Your name",max_length=20,error_messages={
-        "required":"Your must not be empty",
-        "max_length":"Please Enter Shoter Name"
-    })
-    review_text =forms.CharField(label="Your feedback",widget=forms.Textarea,max_length=200)
-    rating=forms.IntegerField(label="Your Rating",min_value=1,max_value=5)
+# class ReviewForm(forms.Form):
+#     user_name=forms.CharField(label="Your name",max_length=20,error_messages={
+#         "required":"Your must not be empty",
+#         "max_length":"Please Enter Shoter Name"
+#     })
+#     review_text =forms.CharField(label="Your feedback",widget=forms.Textarea,max_length=200)
+#     rating=forms.IntegerField(label="Your Rating",min_value=1,max_value=5)
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model= Review
+        fields ='__all__'
+        # exclude= " djjd jsj field name" if you want to excludeRe
