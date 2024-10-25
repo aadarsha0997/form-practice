f743951608d339d0e61ef33afda7eaf18a0f6d08
Ayush <aadarsha0997@gmail.com>
2024-10-25 16:37:12 +0545

    Complete of form and class base view section with different type of special view

--- reviews/urls.py
+++ reviews/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     # path("thank-you",views.thank_you,name="thank-you")   function base view of thankyou post
     path("thank-you",views.ThankYouView.as_view()),
     path("review-list",views.ReviewsListView.as_view()),
-    path("review-list/<int:id>",views.DetailView.as_view()),
+    path("review-list/<int:pk>",views.DetailView.as_view()),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -3,29 +3,50 @@ from django.http import HttpResponseRedirect
 from .forms import ReviewForm
 from .models import Review
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import ListView,DetailView,FormView,TemplateView,CreateView
 
 
 
 # Create your views here.
 
-####### class base view #######
-class ReviewView(View):
-    def get(self,request):
-        form = ReviewForm()
-        return render(request, "reviews/review.html",{
-            "form": form
-        })
+####### class base view extemd from basic view #######
+# class ReviewView(View):
+#     def get(self,request):
+#         form = ReviewForm()
+#         return render(request, "reviews/review.html",{
+#             "form": form
+#         })
     
 
-    def post(self,request):
-        form= ReviewForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("thank-you")
-        return render(request,"reviews/review.html",{
-            "form": form
-        })
+#     def post(self,request):
+#         form= ReviewForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect("thank-you")
+#         return render(request,"reviews/review.html",{
+#             "form": form
+#         })
+
+####### class base view extemd from formview #######
+# class ReviewView(FormView):
+#     form_class= ReviewForm  ### form name assining
+#     template_name="reviews/review.html"
+#     success_url ="/thank-you"
+
+#### extending from creat view to save form  #### 
+class ReviewView(CreateView):
+    model= Review  #### pointing our model
+    form_class= ReviewForm  ### pointing our form
+    template_name="reviews/review.html"
+    success_url="/thank-you"
+
+#### form saving in this formview
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+    
+
+
         ##### function base view ######
 # def review(request):
 #     if request.method== 'POST':
@@ -63,27 +84,50 @@ class ReviewView(View):
 
 ####### class base view but specific for returning templete ####
 class ThankYouView(TemplateView):
+
     template_name="reviews/thank-you.html"
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["message"] = "This  really works"
         return context
+######### extending from templateview ####
+# class ReviewsListView(TemplateView):
+#     template_name="reviews/review-list.html"
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         review= Review.objects.all()
+#         context["reviews"] = review
+#         return context
 
-class ReviewsListView(TemplateView):
-    template_name="reviews/review-list.html"
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        review= Review.objects.all()
-        context["reviews"] = review
-        return context
-class DetailView(TemplateView):
+##### same template extending from listview because it is more specific #######
+
+class ReviewsListView(ListView):
+    template_name ="reviews/review-list.html"
+    model = Review  ### pointing to model from which it have to fetch data
+    context_object_name = "reviews"  #### assining name to it to get data in template / by defult it is 0bject_list
+
+    def get_queryset(self):
+
+        base_quary=super().get_queryset()
+        data=base_quary.filter(rating__gt=4) #### only show review greater than 4 i.e 5
+        return data
+    
+##### extending from templateview ####
+# class DetailView(TemplateView):
+#     template_name="reviews/detail-view.html"
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         selected_id=kwargs["id"]
+#         selected_review=Review.objects.get(pk=selected_id)
+#         context["review"] = selected_review
+#         return context
+
+
+##### extending from detailview ####
+class DetailView(DetailView):
     template_name="reviews/detail-view.html"
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        selected_id=kwargs["id"]
-        selected_review=Review.objects.get(pk=selected_id)
-        context["review"] = selected_review
-        return context
+    model=Review
+
     
     
     
