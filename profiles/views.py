afa93bba45a66a805624caec53b2cba31172e042
Ayush <aadarsha0997@gmail.com>
2024-12-15 13:33:35 +0545

    ready to receive file now need to stor and present

--- profiles/views.py
+++ profiles/views.py
@@ -3,16 +3,12 @@ from django.views import View
 from django.http import HttpResponseRedirect
 
 # Create your views here.
-def store_file(file):
-    with open("temp/image.jpg","wb+") as dest:
-        for chunk in file.chunks():   ###### chunk is build in method; More on official doc
-            dest.write(chunk)
+
 
 class CreateProfileView(View):
     def get(self, request):
         return render(request, "profiles/create_profile.html")
 
     def post(self, request):
-     #   print(request.FILES["image"]) ### image is the name we submit to form to identify from which it should take FILE
-        store_file(request.FILES["image"]) 
+        print(request.FILES["image"])# image is name assign to form's input cause there can be multiple file uplodes in same form
         return HttpResponseRedirect("/profiles")
